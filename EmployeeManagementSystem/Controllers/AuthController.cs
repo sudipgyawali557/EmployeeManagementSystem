ddefd9efea42a66969f54a9e358fbe09f0250b7a
sudipgyawali <sudipgyawali27@gmail.com>
2022-06-20 08:26:37 +0545

    first commit

---
+++ EmployeeManagementSystem/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ViewResult MainPage()
+        {
+            return View();
+        }
+        public ViewResult Login()
+        {
+            return View();
+        }
+        public ViewResult Create()
+        {
+            return View();
+        }
+        public ViewResult Display()
+        {
+            return View();
+        }
+        public ViewResult Details()
+        {
+            return View();
+        }
+        public ViewResult Departments()
+        {
+            return View();
+        }
+    }
+}
